03928aafd31e0f1ad382b85d10f21cbe6e3f8aa1
marcos-cmc <marcos.dominguez@grupocmc.es>
2020-11-23 10:46:51 +0100

    Fin

---
+++ Comun_DB/CIEDw/dbo/Views/z_Fact_Presupuesto.sql
@@ -0,0 +1,18 @@
+﻿
+
+
+
+
+
+
+
+CREATE view [dbo].[z_Fact_Presupuesto] as 
+select 
+Planta
+, Sociedad
+, cast ( cast(Ano*10000+101 as char(8)) as date) as fecha
+, Orden
+, Presupuesto
+, PptoInicial
+from 
+[dbo].[z_SAPPresupuesto]
